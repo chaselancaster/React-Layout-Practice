01f97e6c3e97a52c5d09c5c6792a519d57e82200
Chase Lancaster <chaseblancaster@gmail.com>
2021-02-17 14:00:58 -0800

    create Header.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 
 import NavBar from "./Component/NavBar/NavBar";
+import Header from "./Component/Header/Header";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <Header />
     </div>
   );
 }
