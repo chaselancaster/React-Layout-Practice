dc56bc9685f650142beca646147b6c6d4ddf5dee
Chase Lancaster <chaseblancaster@gmail.com>
2021-02-17 13:43:44 -0800

    add practice NavBar initial layout

---
+++ src/Component/NavBar/NavBar.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const NavBar = () => {
+  return (
+    <div>
+      <ul>
+        <li>Home</li>
+        <li>About Me</li>
+        <li>Projects</li>
+        <li>Contact</li>
+      </ul>
+    </div>
+  );
+};
+
+export default NavBar;
